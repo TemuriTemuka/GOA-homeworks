c4c5f65616c53c93ec18d937f4ef71dfc117b268
TemuriTemuka <temukainaishvili91@gmail.com>
2025-05-09 12:54:06 +0400

    goa

---
+++ day4/classwork/lesson4.py
@@ -0,0 +1,9 @@
+name= 'Temuka'
+surname = 'inaishvili'
+age = 15
+hobby = 'rugby play'
+pozition = 5
+
+
+
+print("hello,my name is(name)and my surname is (surname),i am (age) years old, my favorite hobby is hobby, my pozition is (pozition).")
---
+++ day4/homework/lesson4.py
@@ -0,0 +1,24 @@
+#1
+name = input("Enter your name: ")
+print(name)
+age = int(input("Enter your age: "))
+print(age)
+#2
+num =int(input("Enter width number: "))
+num1 = int(input("Enter height number: "))
+p = (num + num1) * 2
+print(p)
+
+S = num * num1
+print(S)
+
+
+# input- ყველა ინფორმაცია რა კომპიუტერში შედის
+#output - ინგოფორმაცია რასაც კომპიუტერი გვაწვდის
+
+num2 = int(input("Enter random number: "))
+num3 = int(input("Enter random number: "))
+print(num2 + num3)
+print(num2 - num3)
+print(num2 * num3)
+print(num2 / num3)
